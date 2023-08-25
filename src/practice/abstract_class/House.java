8b75081466cbed8f66c9d8e5f990c2a524c64055
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-25 13:13:15 +0200

    init commit

---
+++ src/practice/abstract_class/House.java
@@ -0,0 +1,11 @@
+package practice.abstract_class;
+
+public interface House {
+    //принять на довольствие
+    boolean addPet();
+    //снять с довольствия
+    Pet removePet(Pet pet);
+    //закупить всем еду
+    //гигиена
+
+}
